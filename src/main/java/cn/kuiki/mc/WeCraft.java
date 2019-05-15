1aeddded6d67ed5e5f16734e0adcbde5343fec0c
Kuiki <ikuiki360@gmail.com>
2019-05-15 06:00:16 +0000

    添加spigot插件基本架构

--- src/main/java/cn/kuiki/mc/WeCraft.java
+++ src/main/java/cn/kuiki/mc/WeCraft.java
@@ -1,11 +1,15 @@
 package cn.kuiki.mc;
 
-/**
- * Hello world!
- *
- */
-public class WeCraft {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WeCraft extends JavaPlugin {// 继承类
+    @Override
+    public void onEnable() {
+        // 启用插件时自动发出
+    }
+
+    @Override
+    public void onDisable() {
+        // 关闭插件时自动发出
     }
 }
