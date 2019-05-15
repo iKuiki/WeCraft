8309dfd548a5258401ffbf1867a6009b7d4ccf04
Kuiki <ikuiki360@gmail.com>
2019-05-15 02:00:32 +0000

    添加maven初始项目

---
+++ src/main/java/cn/kuiki/mc/WeCraft.java
@@ -0,0 +1,11 @@
+package cn.kuiki.mc;
+
+/**
+ * Hello world!
+ *
+ */
+public class WeCraft {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/cn/kuiki/mc/WeCraftTest.java
@@ -0,0 +1,33 @@
+package cn.kuiki.mc;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple WeCraft.
+ */
+public class WeCraftTest extends TestCase {
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public WeCraftTest(String testName) {
+        super(testName);
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite() {
+        return new TestSuite(WeCraftTest.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testWeCraft() {
+        assertTrue(true);
+    }
+}
